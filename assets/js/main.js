bbd854106dca6a39dae05e4c7321add475e990dc
DanOukolov <oukolovdaniel@gmail.com>
2022-10-05 08:27:25 -0400

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,4 @@
 
 /* javascript */
-console.log()
+console.log("This work was designed by the Lemonheads, an American alt rock band.");
+console.log("This design was made by Daniel Oukolov as a responsive web page.");
